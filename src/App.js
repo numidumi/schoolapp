5c33e939e3dd7ba2a170ed6ea1f120ae8e7ff9b8
Atakan Celik <atakan.celik@student.htldornbirn.at>
2023-03-21 09:33:27 +0100

    footer 2

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./App.css";
 import Cards from "./components/Students/Cards.js";
 import Header from "./components/Header/Header.js";
+import Footer from "./components/Footer/Footer.js";
 import Home from "./components/subpages/Home.js";
 import Teachers from "./components/subpages/Teachers.js";
 import Students from "./components/Students/Students.js";
@@ -11,6 +12,7 @@ function App() {
     <div className="App">
       <Header />
       <Students />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Footer.css"
+
+const footer = () => {
+  return (
+    <div>
+      <footer className="footer">
+        <h1 className="footer_title">Schuljahr</h1>
+        <h1 className="year">2023</h1>
+      </footer>
+    </div>
+  );
+};
+
+export default footer;
