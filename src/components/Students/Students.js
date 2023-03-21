7df742aab6edeae98020218719083d53cc3689b3
metehan <metehan.altundal@student.htldornbirn.at>
2023-03-21 09:36:11 +0100

    grid

--- src/components/Students/Students.js
+++ src/components/Students/Students.js
@@ -18,131 +18,164 @@ import serbia from "./images/serbia.png";
 import turkey from "./images/turkey.png";
 import austria from "./images/austria.png";
 import Cards from "./Cards.js";
+import Grid from "@mui/material/Grid";
 
 const Students = () => {
   return (
-    <div className="main">
-      <Cards
-        name="Metehan"
-        lastName="Altundal"
-        desc="Metehan ist 18 Jahre alt und kommt aus Dornbirn. 
+    <div className="wrapper">
+      <Grid container spacing={1}>
+        <Grid item xs={10} md={5} lg={2.8}>
+          <Cards
+            name="Metehan"
+            lastName="Altundal"
+            desc="Metehan ist 18 Jahre alt und kommt aus Dornbirn. 
               Die Geschichte ist sein Spezialgebiet."
-        colors="blue"
-        image={metehan}
-        flag={turkey}
-      />
-      <Cards
-        name="Florian"
-        lastName="Blum"
-        desc="Florian ist 18 und wurde in Dornbirn geboren."
-        colors="orange"
-        image={flo}
-        flag={austria}
-      />
-      <Cards
-        name="Mathias"
-        lastName="Buchwald"
-        desc="Mathias ist 18 und wurde in Dornbirn geboren."
-        colors="brown"
-        image={mathias}
-        flag={austria}
-      />
-      <Cards
-        name="Atakan"
-        lastName="Celik"
-        desc="Atakan ist 19 und ist gebürtiger Dornbirner. Er ist der beste Kickboxer."
-        colors="green"
-        image={atakan}
-        flag={turkey}
-      />
-      <Cards
-        name="Christoph"
-        lastName="Derigo"
-        desc="Christoph ist 19 und wurde in Dornbirn geboren. In seiner Freizeit spielt er gerne Volleyball."
-        colors="green"
-        image={chris}
-        flag={austria}
-      />
-      <Cards
-        name="Numan"
-        lastName="Erter"
-        desc="Numan ist 18 und wurde in Dornbirn geboren."
-        colors="brown"
-        image={numan}
-        flag={turkey}
-      />
-      <Cards
-        name="Yannik"
-        lastName="Fischer"
-        desc="Yannik ist 19 und wurde in Dornbirn geboren."
-        colors="orange"
-        image={yannik}
-        flag={austria}
-      />
-      <Cards
-        name="Sebastian"
-        lastName="Gärtner"
-        desc="Sebastian ist 19 und wurde in Dornbirn geboren."
-        colors="blue"
-        image={sebi}
-        flag={austria}
-      />
-      <Cards
-        name="Sven"
-        lastName="Hartmann"
-        desc="Sven ist 18 und wurde in Dornbirn geboren."
-        colors="blue"
-        image={sven}
-        flag={austria}
-      />
-      <Cards
-        name="Niclas"
-        lastName="Kostelac"
-        desc="Niclas ist 18 und wurde in Dornbirn geboren."
-        colors="orange"
-        image={niclas}
-        flag={austria}
-      />
-      <Cards
-        name="Kai"
-        lastName="Mayer"
-        desc="Kai ist 19 und wurde in Dornbirn geboren."
-        colors="brown"
-        image={kai}
-        flag={austria}
-      />
-      <Cards
-        name="Kristijan"
-        lastName="Mladenovic"
-        desc="Kristijan ist 19 und kommt aus Hohenems. Er liebt es zu programmieren."
-        colors="green"
-        image={kris}
-        flag={serbia}
-      />
-      <Cards
-        name="Matthias"
-        lastName="Tschmelitsch"
-        desc="Matthias ist 18 und wurde in Dornbirn geboren."
-        colors="green"
-        image={matthias}
-        flag={austria}
-      />
-      <Cards
-        name="Fabian"
-        lastName="Türtscher"
-        desc="Fabian ist 18 und wurde in Dornbirn geboren."
-        colors="brown"
-        image={fabian}
-        flag={austria}
-      />
-      <Cards
-        name="Elias"
-        lastName="Wohlgenannt"
-        desc="Elias ist 19 und wurde in Dornbirn geboren."
-        colors="orange"
-        image={elias}
-        flag={austria}
-      />
+            colors="blue"
+            image={metehan}
+            flag={turkey}
+          />
+        </Grid>
+        <Grid item xs={10} md={5} lg={2.8}>
+          <Cards
+            name="Florian"
+            lastName="Blum"
+            desc="Florian ist 18 und wurde in Dornbirn geboren."
+            colors="orange"
+            image={flo}
+            flag={austria}
+          />
+        </Grid>
+        <Grid item xs={10} md={5} lg={2.8}>
+          <Cards
+            name="Mathias"
+            lastName="Buchwald"
+            desc="Mathias ist 18 und wurde in Dornbirn geboren."
+            colors="brown"
+            image={mathias}
+            flag={austria}
+          />
+        </Grid>
+        <Grid item xs={10} md={5} lg={2.8}>
+          <Cards
+            name="Atakan"
+            lastName="Celik"
+            desc="Atakan ist 19 und ist gebürtiger Dornbirner. Er ist der beste Kickboxer."
+            colors="green"
+            image={atakan}
+            flag={turkey}
+          />
+        </Grid>
+        <Grid item xs={10} md={5} lg={2.8}>
+          <Cards
+            name="Christoph"
+            lastName="Derigo"
+            desc="Christoph ist 19 und wurde in Dornbirn geboren. In seiner Freizeit spielt er gerne Volleyball."
+            colors="green"
+            image={chris}
+            flag={austria}
+          />
+        </Grid>
+        <Grid item xs={10} md={5} lg={2.8}>
+          <Cards
+            name="Numan"
+            lastName="Erter"
+            desc="Numan ist 18 und wurde in Dornbirn geboren."
+            colors="brown"
+            image={numan}
+            flag={turkey}
+          />
+        </Grid>
+        <Grid item xs={10} md={5} lg={2.8}>
+          <Cards
+            name="Yannik"
+            lastName="Fischer"
+            desc="Yannik ist 19 und wurde in Dornbirn geboren."
+            colors="orange"
+            image={yannik}
+            flag={austria}
+          />
+        </Grid>
+        <Grid item xs={10} md={5} lg={2.8}>
+          <Cards
+            name="Sebastian"
+            lastName="Gärtner"
+            desc="Sebastian ist 19 und wurde in Dornbirn geboren."
+            colors="blue"
+            image={sebi}
+            flag={austria}
+          />
+        </Grid>
+        <Grid item xs={10} md={5} lg={2.8}>
+          <Cards
+            name="Sven"
+            lastName="Hartmann"
+            desc="Sven ist 18 und wurde in Dornbirn geboren."
+            colors="blue"
+            image={sven}
+            flag={austria}
+          />
+        </Grid>
+        <Grid item xs={10} md={5} lg={2.8}>
+          <Cards
+            name="Niclas"
+            lastName="Kostelac"
+            desc="Niclas ist 18 und wurde in Dornbirn geboren."
+            colors="orange"
+            image={niclas}
+            flag={austria}
+          />
+        </Grid>
+        <Grid item xs={10} md={5} lg={2.8}>
+          <Cards
+            name="Kai"
+            lastName="Mayer"
+            desc="Kai ist 19 und wurde in Dornbirn geboren."
+            colors="brown"
+            image={kai}
+            flag={austria}
+          />
+        </Grid>
+        <Grid item xs={10} md={5} lg={2.8}>
+          <Cards
+            name="Kristijan"
+            lastName="Mladenovic"
+            desc="Kristijan ist 19 und kommt aus Hohenems. Er liebt es zu programmieren."
+            colors="green"
+            image={kris}
+            flag={serbia}
+          />
+        </Grid>
+        <Grid item xs={10} md={5} lg={2.8}>
+          <Cards
+            name="Matthias"
+            lastName="Tschmelitsch"
+            desc="Matthias ist 18 und wurde in Dornbirn geboren."
+            colors="green"
+            image={matthias}
+            flag={austria}
+          />
+        </Grid>
+        <Grid item xs={10} md={5} lg={2.8}>
+          <Cards
+            name="Fabian"
+            lastName="Türtscher"
+            desc="Fabian ist 18 und wurde in Dornbirn geboren."
+            colors="brown"
+            image={fabian}
+            flag={austria}
+          />
+        </Grid>
+        <Grid item xs={10} md={5} lg={2.8}>
+          <Cards
+            name="Elias"
+            lastName="Wohlgenannt"
+            desc="Elias ist 19 und wurde in Dornbirn geboren."
+            colors="orange"
+            image={elias}
+            flag={austria}
+          />
+        </Grid>
+      </Grid>
     </div>
   );
 };
