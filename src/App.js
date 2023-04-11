924ebf125c3a7a14fe7fa41ca822a22af68ecc5e
Atakan Celik <atakan.celik@student.htldornbirn.at>
2023-04-11 09:03:33 +0200

    mal

--- src/App.js
+++ src/App.js
@@ -2,10 +2,6 @@ import React from "react";
 import "./App.css";
 import Cards from "./components/Students/Cards.js";
 import Header from "./components/Header/Header.js";
-<<<<<<< HEAD
-=======
-import Footer from "./components/Students/Footer/Footer";
->>>>>>> 3977e1eb1ed37499073bdc559513ab745821dd02
 import Home from "./components/subpages/Home.js";
 import Teachers from "./components/subpages/Teachers.js";
 import Students from "./components/Students/Students.js";
