5b9173f8fe054e2aed154c69019c045923a3655c
metehan <metehan.altundal@student.htldornbirn.at>
2023-04-11 09:06:23 +0200

    header components eingefügt

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from "react";
 import "./App.css";
 import Cards from "./components/Students/Cards.js";
 import Header from "./components/Header/Header.js";
-import Footer from "./components/Footer/Footer.js";
 import Home from "./components/subpages/Home.js";
 import Teachers from "./components/subpages/Teachers.js";
 import Students from "./components/Students/Students.js";
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -7,6 +7,9 @@ const Header = () => {
       <header className="header">
         <h1 className="header__title">HTL</h1>
         <h1 className="dornbirn">Dornbirn</h1>
+        <h1 className="home">Home</h1>
+        <h1 className="students">Students</h1>
+        <h1 className="aboutus">About us</h1>
       </header>
     </div>
   );
